15d2d2bf1692f7cb830d48f1bf54e28b6eecb5ee
Max Davitt <med112@uakron.edu>
2022-01-13 22:24:03 -0500

    Patch OpenSSL's config and hopefully fix everything

--- parse.sh
+++ parse.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-sudo sed -i -e "s/CipherString = DEFAULT:@SECLEVEL=2/CipherString = DEFAULT:@SECLEVEL=1/" /etc/ssl/openssl.cnf
+if [[ -v CI ]]; then
+	sudo patch /etc/ssl/openssl.cnf openssl.cnf.patch
+fi
 
-html=$(wget -O /dev/null -S https://maps.uakron.edu)
-
-printf '%s\n' "$html"
+html=$(curl -s https://maps.uakron.edu)
 
 from_variable() {
-	echo $(printf '%s\n' "$html" | grep "var $1" | sed "s/^[ \t]*//; s/var $1 = //; s/;//")
+	echo $(echo "$html" | grep "var $1" | sed "s/^[ \t]*//; s/var $1 = //; s/;//")
 }
 
 mkdir out
 cd out
 
-from_variable buildings # > buildings.json
-from_variable parkingLots # > parkingLots.json
-from_variable parkingRelations # > parkingRelations.json
-from_variable departments # > departments.json
-from_variable categories # > categories.json
-from_variable buildingCategories # > buildingCategories.json
+from_variable buildings > buildings.json
+from_variable parkingLots > parkingLots.json
+from_variable parkingRelations > parkingRelations.json
+from_variable departments > departments.json
+from_variable categories > categories.json
+from_variable buildingCategories > buildingCategories.json
